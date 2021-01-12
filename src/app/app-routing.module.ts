86cdc2637679054d7c750672992719fa284d4b08
rahim br <berhi93ddich@outlook.com>
2021-01-12 16:51:04 +0100

    cmt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
  
@@ -21,6 +22,7 @@ const routes: Routes = [
 
     {
       path: 'tabs-layout',
+      canActivate: [AuthGuard],
       loadChildren: () => import('./component/layout-home/tabs-layout/tabs-layout.module').then(m => m.TabsLayoutModule),
 
   },
